9e6a39339dd563d50ac0960dd58a48ab77b42062
Austin Baccus <austinbaccus@live.com>
2022-01-15 22:37:42 -0800

    Camera works slightly better.

--- Marooned/Camera.cs
+++ Marooned/Camera.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Marooned.Sprites;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Marooned
 {
@@ -8,11 +9,11 @@ namespace Marooned
         public Matrix Transform { get; private set; }
         public void Follow(Sprite target)
         {
-            float zoom = 2.0f;
+            float zoom = 0.10f; // 0.45f is good
 
             var position = Matrix.CreateTranslation(
-              -target.Position.X - (target.Rectangle.Width / (2 / zoom)),
-              -target.Position.Y - (target.Rectangle.Height / (2 / zoom)),
+              -target.Position.X - (target.Rectangle.Width / 2),
+              -target.Position.Y - (target.Rectangle.Height / 2),
               0);
 
             var offset = Matrix.CreateTranslation(
@@ -20,10 +21,18 @@ namespace Marooned
                 Game1.ScreenHeight / 2,
                 0);
 
-            // https://roguesharp.wordpress.com/2014/07/13/tutorial-5-creating-a-2d-camera-with-pan-and-zoom-in-monogame/
             var scale = Matrix.CreateScale(new Vector3(zoom, zoom, 1));
+            //Transform = position * offset * scale;
 
-            Transform = position * offset * scale;
+            // https://roguesharp.wordpress.com/2014/07/13/tutorial-5-creating-a-2d-camera-with-pan-and-zoom-in-monogame/
+            var pos = Matrix.CreateTranslation(
+                target.Position.X, // - (target.Rectangle.Width / 2), 
+                target.Position.Y, // - (target.Rectangle.Height / 2), 
+                0);
+
+            var transform = scale * pos;
+            Transform = Matrix.Invert(transform);
+
         }
     }
 }
