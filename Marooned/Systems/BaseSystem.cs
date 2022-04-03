f4690223229cf6f35bdb89f164caaa6d79efdc4f
Hekili Jordan <hekili.jordan@wsu.edu>
2022-04-03 14:16:57 -0700

    Created a basesytem

--- Marooned/Systems/BaseSystem.cs
+++ Marooned/Systems/BaseSystem.cs
@@ -1,7 +1,25 @@
-﻿namespace Marooned.Systems
-{
-    public class BaseSystem
-    {
+﻿using Marooned.Components;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 
+namespace Marooned.Systems
+{
+    public class BaseSystem<T> where T: BaseComponent
+    {
+        private List<T> _components = new List<T>();
+        public void Update(GameTime gameTime)
+        {
+            foreach(T component in _components)
+            {
+                component.Update(gameTime);
+            }
+        }
+
+        // add components
+
+        public void AddComponent(T component)
+        {
+            _components.Add(component);
+        }
     }
 }
