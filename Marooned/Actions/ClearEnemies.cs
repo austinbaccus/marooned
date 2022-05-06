7e06be6f1ddd521091cbeee4f79ef65dbdedc7c8
Odeysiuss Tuon <odeysiuss.tuon@wsu.edu>
2022-05-06 12:41:59 -0700

    Added ClearEnemies action

---
+++ Marooned/Actions/ClearEnemies.cs
@@ -0,0 +1,23 @@
+﻿using DefaultEcs;
+using Marooned.Components;
+
+namespace Marooned.Actions
+{
+    public class ClearEnemies
+    {
+        private readonly World _world;
+
+        public ClearEnemies(World world)
+        {
+            _world = world;
+        }
+
+        public void Execute(GameContext gameContext)
+        {
+            foreach (Entity entity in _world.GetEntities().With<IsEnemyComponent>().AsSet().GetEntities())
+            {
+                entity.Dispose();
+            }
+        }
+    }
+}
